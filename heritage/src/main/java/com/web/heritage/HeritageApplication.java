24e3810953e837d05507f8aacb8ad6e461363e14
skeldtcan <skeldtcan@daum.net>
2021-01-19 11:08:09 +0900

    Feat: 프로젝트 생성

---
+++ heritage/src/main/java/com/web/heritage/HeritageApplication.java
@@ -0,0 +1,13 @@
+package com.web.heritage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HeritageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HeritageApplication.class, args);
+	}
+
+}
