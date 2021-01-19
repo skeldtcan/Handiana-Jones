9741e3e8bc194261984a17b87ed945af08a1d6ea
cha <cha@DESKTOP-VHS2TH0>
2021-01-19 15:29:18 +0900

    aa

--- heritage/src/main/java/com/web/heritage/HeritageApplication.java
+++ heritage/src/main/java/com/web/heritage/HeritageApplication.java
@@ -1,5 +1,9 @@
 package com.web.heritage;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+import org.junit.jupiter.api.Test;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -9,5 +13,18 @@ public class HeritageApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(HeritageApplication.class, args);
 	}
+	
+	@Test
+	public void testConnection() throws Exception{
+		Class.forName(DRIVER);
+		try(Connection connection = DriverManager.getConnection(URL, USER, PASSWORD)) {
+			System.out.println(connection);
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+	}
+	
 
 }
