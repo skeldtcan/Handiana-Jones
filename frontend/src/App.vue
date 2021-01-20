6b1b3f83b99d8ee401326401c8218b566ca3b15a
multicampus <brotherspear106@gmail.com>
2021-01-20 18:01:09 +0900

    Fix: MR conflict 해결을 위한 커밋

---
+++ frontend/src/App.vue
@@ -0,0 +1,42 @@
+<template>
+  <v-app>
+    <!-- Header.vue를 이용한 헤더 부분 -->
+    <Header></Header>
+     <!-- Main.vue를 이용한 메인 부분  -->
+    <v-main>
+      <Main/>
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+import Header from './components/common/Header';
+import Main from './components/common/Main';
+
+export default {
+  name: 'App',
+
+  components: {
+    Header,
+    Main,
+  },
+
+  data: () => ({
+    //
+  }),
+};
+</script>
+
+<style>
+/* east-sea-Dokdo font import */
+@import url('https://fonts.googleapis.com/css2?family=East+Sea+Dokdo&display=swap');
+/* Gugi font import */
+@import url('https://fonts.googleapis.com/css2?family=Gugi&display=swap');
+.east-sea-Dokdo {
+  font-family: 'East Sea Dokdo', cursive;
+}
+.gugi {
+  font-family: 'Gugi', cursive;
+}
+
+</style>
---
+++ frontend/src/components/modal/auth/Join.vue
@@ -0,0 +1,101 @@
+<template>
+  <v-row justify="">
+
+    <v-col cols="auto">
+      <v-dialog
+        transition="dialog-bottom-transition"
+        max-width="600"
+      >
+        <template v-slot:activator="{ on, attrs }">
+          <v-btn
+            color="brownLighten4"
+            v-bind="attrs"
+            v-on="on"
+          ><span class="blue-grey--text text--darken-4 gugi" style="font-size:20px; padding: 10px; ">회원가입</span></v-btn>
+        </template>
+        <template v-slot:default="dialog">
+          <v-card>
+            <v-toolbar
+            class="mb-4"
+              color="blueGreyDarken4"
+              dark
+            ><span class="white--text east-sea-Dokdo " style="font-size:48px;">회원가입</span></v-toolbar>
+            <v-card-text>
+              <br>
+              <v-text-field
+              v-model="user.userEmail"
+              outlined
+            label="이메일"
+          ></v-text-field>
+          <v-text-field
+          type="password"
+          outlined
+          v-model="user.userPwd"
+            label="비밀번호"
+          ></v-text-field>
+          <v-text-field
+          type="password"
+          outlined
+          v-model="user.userPwdCon"
+            label="비밀번호확인"
+          ></v-text-field>
+          <v-text-field
+          v-model="user.userPhone"
+          outlined
+            label="연락처"
+          ></v-text-field>
+            </v-card-text>
+            <v-card-actions class="justify-end">
+              <v-btn
+                color="brownLighten4"
+                @click="create"
+              ><span class="blue-grey--text text--darken-4 gugi" style="font-size:15px; padding: 10px; ">회원가입</span></v-btn>
+              <v-btn
+                text
+                @click="dialog.value = false"
+              ><span class="blue-grey--text text--darken-4 gugi" style="font-size:15px; padding: 10px; ">창닫기</span></v-btn>
+            </v-card-actions>
+          </v-card>
+        </template>
+      </v-dialog>
+    </v-col>
+  </v-row>
+  
+</template>
+
+<script>
+export default {
+    name: 'join',
+    data(){
+      return {
+        user:{
+          userEmail:null,
+          userPwd:null,
+          userPwdCon:null,
+          userPhone:null,
+        },
+
+      }
+    },
+    computed:{
+      
+    },
+    methods:{
+      create(){
+        console.log(this.user.userEmail)
+        console.log(this.user.userPwd)
+        console.log(this.user.userPwdCon)
+        console.log(this.user.userPhone)
+        if (this.user.userPwd===this.user.userPwdCon){
+          console.log('비밀번호가 옳습니다')
+        } else {
+          console.log('비밀번호가 틀립니다')
+        }
+      }
+    },
+}
+</script>
+
+<style>
+
+</style>
