52a83a39732cac36f8866759a7567d74d042c733
multicampus <multicampus@DESKTOP-KVCQHCD>
2021-01-26 18:55:04 +0900

    Feat: user branch pull & 이메일 인증(smtp) 기능 추가

---
+++ heritage/src/main/java/com/web/heritage/config/MailConfig.java
@@ -0,0 +1,26 @@
+package com.web.heritage.config;
+
+import java.util.Properties;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
+
+@Configuration
+public class MailConfig {
+	@Bean
+	public static JavaMailSender mailSender() {
+		JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
+		mailSender.setHost("smtp.gmail.com");
+		mailSender.setPort(587);
+		mailSender.setUsername("doekchl1124@gmail.com");
+		mailSender.setPassword("zkolqgffvrqcxocb");
+		mailSender.setDefaultEncoding("utf-8");
+		Properties javaMailProperties = new Properties();
+		javaMailProperties.put("mail.smtp.auth", "true");
+		javaMailProperties.put("mail.smtp.starttls.enable", "true");
+		javaMailProperties.put("mail.smtp.ssl.trust", "smtp.gmail.com");
+		return mailSender;
+	}
+}
