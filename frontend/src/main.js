b08e70711ddb1e99a26bd9238f667061b1e21fd7
이형창(Hyung Chang Lee) <68407561+brotherspear1994@users.noreply.github.com>
2021-02-16 11:50:04 +0900

    Feat: 알고리즘 추천 목록페이지 Gilde+Card 폼 완성

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -2,8 +2,11 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import vuetify from './plugins/vuetify';
+import VueGlide from 'vue-glide-js'
+import 'vue-glide-js/dist/vue-glide.css'
 import store from './store'
 
+Vue.use(VueGlide)
 Vue.config.productionTip = false
 
 new Vue({
